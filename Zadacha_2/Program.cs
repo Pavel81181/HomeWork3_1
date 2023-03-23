779badaa11463609a8f92ebd01199c63afec50f5
Pavel Bazhin <p81181@yandex.ru>
2023-03-23 23:19:16 +0300

    Написал программу для решения задачи №4

--- Zadacha_2/Program.cs
+++ Zadacha_2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача № 4
+// Программа для ввода трех чисел и поиска наибольшего
+
+Console.Clear ();
+Console.Write("Введите число А:");
+string strNumA = Console.ReadLine();
+int numA = Convert.ToInt32(strNumA);
+Console.Write("Введите число B:");
+string strNumB = Console.ReadLine();
+int numB = Convert.ToInt32(strNumB);
+Console.Write("Введите число С:");
+string strNumC = Console.ReadLine();
+int numC = Convert.ToInt32(strNumA);
+int max = numA;
+
+if (numB > max)
+{
+    max = numB;
+}
+if (numC > max)
+{
+    max = numC;
+}
+Console.WriteLine($"Максимальное из введенных число {max}");
