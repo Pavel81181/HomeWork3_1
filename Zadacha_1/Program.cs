99246a3440d063f6b78b394054c190e5352fa978
Pavel Bazhin <p81181@yandex.ru>
2023-03-23 22:57:47 +0300

    Написал программу для решения 2-й задачи

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача № 2 
+// Программа для ввода двух чисел и поиска наибольшего
+
+Console.Clear ();
+Console.Write("Введите число А:");
+string strNumA = Console.ReadLine();
+int numA = Convert.ToInt32(strNumA);
+Console.Write("Введите число B:");
+string strNumB = Console.ReadLine();
+int numB = Convert.ToInt32(strNumB);
+if (numA > numB)
+{
+    Console.WriteLine($"Число А = {numA} больше чем число В = {numB}");
+}
+else
+{
+    Console.WriteLine($"Число B = {numB} больше чем число A = {numA}");
+}
