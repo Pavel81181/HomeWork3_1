322520db90a1053a7d758f11b864abbdc6278c7e
Pavel Bazhin <p81181@yandex.ru>
2023-03-24 17:00:22 +0300

    Написал программу для решения задачи № 8

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача № 8
+// Программа для вывода всех четных чисел от 1 до N
+
+Console.Clear ();
+Console.Write("Введите число N: ");
+string strNumN = Console.ReadLine();
+int numN = Convert.ToInt32(strNumN);
+int count = 1;
+Console.WriteLine($"Последовательных четных чисел от 1 до {numN}:");
+while (count <= numN)
+{
+    if  (count%2 == 0)
+    {
+       Console.Write($" {count}_");
+    }
+count++;
+}
+Console.WriteLine();
