4a1f293ed3cff16a61b20a0b342aa6b1aaab4012
Pavel Bazhin <p81181@yandex.ru>
2023-03-24 16:38:51 +0300

    Написал программу для решения задачи №6

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача № 6 
+// Программа для определения четного числа
+
+Console.Clear ();
+Console.Write("Введите число: ");
+string strNum = Console.ReadLine();
+int num = Convert.ToInt32(strNum);
+if (num%2 == 0)
+{
+    Console.WriteLine($"Число  {num} четное");
+}
+else
+{
+    Console.WriteLine($"Число  {num} нечетное");;
+}
